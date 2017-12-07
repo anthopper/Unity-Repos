d3b435180e2120ba03b7564ce8a295fd8f939149
Kit Martin <Kitcmartin@gmail.com>
2017-12-07 06:37:45 -0600

    Useful 2D event Scripts

---
+++ LevelEnd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+	public class LevelEnd : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Player")
+			{
+				SceneManager.LoadScene(SceneManager.GetSceneAt(1).name);
+			}
+		}
+	}
+}
+
---
+++ LevelchangeTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelchangeTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		StartCoroutine(Example());
+	}
+	
+	IEnumerator Example() {
+		print(Time.time);
+		yield return new WaitForSeconds(4);
+		Application.LoadLevel(1);
+	}
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
