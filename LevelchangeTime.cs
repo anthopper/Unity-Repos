520b5bd66b6c5fabc6a4f8a4971a67ca3d168d97
Kit Martin <Kitcmartin@gmail.com>
2019-11-20 18:11:25 -0600

    ChangeScenes

--- LevelchangeTime.cs
+++ LevelchangeTime.cs
@@ -1,23 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelchangeTime : MonoBehaviour {
+public class LevelchangeTime : MonoBehaviour
+{
+    public int sceneNumber; //input the number of the scene in the build order.
+    public int waitTimebeforeSceneLoad; //how many seconds should we wait before loadingthe scene
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
 
-	// Use this for initialization
-	void Start() {
-		StartCoroutine(Example());
-	}
-	
-	IEnumerator Example() {
-		print(Time.time);
-		yield return new WaitForSeconds(4);
-		Application.LoadLevel(1);
-	}
-	
+    IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(waitTimebeforeSceneLoad);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneNumber);
+    }
 
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
